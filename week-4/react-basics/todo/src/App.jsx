e5f69d0e7ae230659c13f536c52043e99006cf32
Chhatrapalsinh Zala <chhatrapalsinh.zala@nitorinfotech.com>
2024-01-08 10:40:57 +0530

    app.jsx file

--- week-4/react-basics/todo/src/App.jsx
+++ week-4/react-basics/todo/src/App.jsx
@@ -2,33 +2,70 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+let globalId = 1
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState({ title: '', description: '' });
+  const [todos, setTodos] = useState([]);
+
+  const inputChangeHandler = (event) => {
+    console.log("event.target",event.target)
+    const key = event.target.id;
+    const value = event.target.value;
+    // console.log("key, value",key, value)
+    setInput((prev) => {
+      return {...prev, [key] : value}
+    }) 
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault()
+    const title = input.title
+    const description = input.description
+    console.log(title, description)
+    const id = globalId
+    setTodos((prev) => {
+      return [...prev, {id, title, description}]
+    })
+    setInput({title : '', description : ''})
+    globalId++ 
+  }
+
 
   return (
-    <>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input  
+          type="text"
+          id='title'
+          placeholder='Todo Title' 
+          value={input.title} 
+          onChange={inputChangeHandler}/>
+          {' '}<br /><br />
+        <input  
+          type="text" 
+          id='description' 
+          placeholder='Description' 
+          value={input.description}
+          onChange={inputChangeHandler}
+        />
+        <br />
+        <br />
+        <button onClick={addTodo}>Add Todo </button>
+        <br />
+        <br />
+        {
+          todos.map((todo) => {
+            console.log("todo", todo)
+            return (
+              <div key={todo.id} id={todo.id}>
+                <div>{todo.title}</div>
+                <div>{todo.description}</div>
+              </div>
+            )
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
